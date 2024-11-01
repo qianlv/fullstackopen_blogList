3a05630431795f4fccb04c6556465c939a9a9cb9
qianlv <qianlv7@qq.com>
2024-11-01 17:24:36 +0800

    Exercises 4.11 - 4.12

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 mongoose.connect(config.MONGODB_URI)
@@ -19,6 +20,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
+app.use(middleware.errorHandler)
 
 
 module.exports = app
--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -68,6 +68,62 @@ test('Add one blogs', async () => {
   assert.strictEqual(all.body.length, testHelper.blogs.length + 1)
 })
 
+test(
+  'Add one blogs missing likes property, it will set to default value 0',
+  async () => {
+    const blog = {
+      title: 'Test Add one Blog',
+      author: 'Test Author',
+      url: 'http://example.com',
+    }
+    const res = await api
+      .post('/api/blogs')
+      .send(blog)
+      .set('Content-Type', 'application/json')
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const returnedBlog = res.body
+    delete returnedBlog.id
+    blog.likes = 0
+    assert.deepStrictEqual(returnedBlog, blog)
+  }
+)
+
+test(
+  'Add one blog missing title, the request will 400 Bad Request',
+  async () => {
+    const blog = {
+      author: 'Test Author',
+      url: 'http://example.com',
+      likes: 10,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blog)
+      .set('Content-Type', 'application/json')
+      .expect(400)
+  }
+)
+
+test(
+  'Add one blog missing url, the request will 400 Bad Request',
+  async () => {
+    const blog = {
+      title: 'Test Add one Blog',
+      author: 'Test Author',
+      likes: 10,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blog)
+      .set('Content-Type', 'application/json')
+      .expect(400)
+  }
+)
+
 after(async () => {
   await mongoose.connection.close()
 })
